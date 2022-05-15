b5b092bf0ede92bda9182e61f76f2c49bc661f33
ValentinGandon <valentin.gandon@epitech.eu>
2022-05-15 21:53:07 +0100

    remove default name

--- src/app/dashboard/app.component.ts
+++ src/app/dashboard/app.component.ts
@@ -21,7 +21,7 @@ export class DashboardComponent implements OnInit {
 
   date!: Date;
   ngOnInit(): void {
-    this.user = "Valentin Gandon";
+    this.user = "";
 
     this.TodoTaskList = [];
     this.DoingTaskList = [];
